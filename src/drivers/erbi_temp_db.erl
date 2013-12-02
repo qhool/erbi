621869785ad7d58d23e49a31622ba0f77322bf85
Cristina Ruiz Corral <ccorral@voalte.com>
2013-12-02 16:17:35 +0100

    Removed comment regarding a previous version

--- src/drivers/erbi_temp_db.erl
+++ src/drivers/erbi_temp_db.erl
@@ -3,18 +3,6 @@
 -module(erbi_temp_db).
 -include("erbi.hrl").
 
-
-%% @doc Starts driver autoprovision if needed.
-%%
-%% When mocking a driver, this function is called each time a connect is requested.
-%% The implementation of ths function is expected to cover:
-%%   - DB instance creation: Some drivers may require creating a separate db instance
-%%       for use of mocking db only. The instance creation should be performed only
-%%       if it was not previouly done.
-%%   - DB initialization: Creating schema, tables...
-%%   - Returning proplist, user and password to be passed to the base driver
-%%       connection request, that allows to connect to the created instance.  
-%%
 -callback start_temp(PropList::[property()])->
     ok.
 
